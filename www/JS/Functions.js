0fa633d330ca42498aab2b0b05f5296dbb44ab10
Daniel Alonso <a26129@svalero.com>
2022-12-11 11:32:27 +0100

    Funcion generar contraseña aleatoria

--- www/JS/Functions.js
+++ www/JS/Functions.js
@@ -1,18 +1,15 @@
-
-
-
 fetch("http://localhost:3000/categories")
     .then(res => res.json())
     .then(data => drawData(data));
 
- fetch("http://localhost:3000/sites")
+fetch("http://localhost:3000/sites")
     .then(res => res.json())
     .then(dataS => sites(dataS));
-  
+
 let categoriesArray = []
 let drawData = (data) => {
     data.forEach(category => {
-        let categoriesAux = new Categories(category.id,category.name,category.createdAt,category.updatedAt)
+        let categoriesAux = new Categories(category.id, category.name, category.createdAt, category.updatedAt)
         categoriesArray.push(categoriesAux)
     })
     DatosCategorias();
@@ -21,14 +18,14 @@ let drawData = (data) => {
 const sitesArray = [];
 let sites = (dataS) => {
     dataS.forEach(sites => {
-        let sitexAux = new Sites(sites.id,sites.name,sites.url,sites.user,sites.password,sites.description,sites.categoryId,sites.createdAt,sites.updatedAt)
+        let sitexAux = new Sites(sites.id, sites.name, sites.url, sites.user, sites.password, sites.description, sites.categoryId, sites.createdAt, sites.updatedAt)
         sitesArray.push(sitexAux)
     })
     DatosSites();
 }
 
-class Categories{
-    constructor(id,name,createdAt,updatedAt){
+class Categories {
+    constructor(id, name, createdAt, updatedAt) {
         this.id = id;
         this.name = name;
         this.createdAt = createdAt;
@@ -36,10 +33,10 @@ class Categories{
     }
 }
 
-class Sites{
-    constructor(id,name,url,user,password,description,categoryId,createdAt,updatedAt){
-        this.id =id;
-        this.name =name;
+class Sites {
+    constructor(id, name, url, user, password, description, categoryId, createdAt, updatedAt) {
+        this.id = id;
+        this.name = name;
         this.url = url;
         this.user = user;
         this.password = password;
@@ -49,57 +46,58 @@ class Sites{
         this.updatedAt = updatedAt;
     }
 }
-function DatosCategorias(){
 
-categoriesArray.map(category => {
-    let ul = document.getElementById('directorioCat');
-    let li = document.createElement('li');
-    li.innerText = category.name;
-    li.onclick = function VerSites(){
-       let tabla= document.querySelectorAll("table tr td")
-       for (let i = 4; i < tabla.length; i++) {
-        tabla[i].remove()
-        }
-    sitesArray.map(site => {
-        if(site.categoryId == category.id){
-        let parent = document.getElementsByTagName('table')[0]
-        let tr = document.createElement('tr')
-        let child = document.createElement('td')
-        let imagen = document.createElement('img')
+function DatosCategorias() {
 
-        child.innerText = site.url
-        tr.appendChild(child)
-        child = document.createElement('td')
+    categoriesArray.map(category => {
+        let ul = document.getElementById('directorioCat');
+        let li = document.createElement('li');
+        li.innerText = category.name;
+        li.onclick = function VerSites() {
+            let tabla = document.querySelectorAll("table tr td")
+            for (let i = 4; i < tabla.length; i++) {
+                tabla[i].remove()
+            }
+            sitesArray.map(site => {
+                if (site.categoryId == category.id) {
+                    let parent = document.getElementsByTagName('table')[0]
+                    let tr = document.createElement('tr')
+                    let child = document.createElement('td')
+                    let imagen = document.createElement('img')
 
-        child.innerText = site.user
-        tr.appendChild(child)
-        child = document.createElement('td')
+                    child.innerText = site.url
+                    tr.appendChild(child)
+                    child = document.createElement('td')
 
-        child.innerText = site.createdAt
-        tr.appendChild(child)
-        child = document.createElement('td')
+                    child.innerText = site.user
+                    tr.appendChild(child)
+                    child = document.createElement('td')
 
-        imagen.src= "https://img.icons8.com/ultraviolet/40/null/opened-folder.png"
-        child.appendChild(imagen)
+                    child.innerText = site.createdAt
+                    tr.appendChild(child)
+                    child = document.createElement('td')
 
-        imagen = document.createElement("img")
-        imagen.src = "https://img.icons8.com/arcade/40/null/delete-sign.png"
-        child.appendChild(imagen)
+                    imagen.src = "https://img.icons8.com/ultraviolet/40/null/opened-folder.png"
+                    child.appendChild(imagen)
 
-        imagen = document.createElement("img")
-        imagen.src = "https://img.icons8.com/3d-plastilina/35/null/pencil--v2.png"
-        child.appendChild(imagen)
-        
-        tr.appendChild(child)
-        parent.appendChild(tr)
-        }
+                    imagen = document.createElement("img")
+                    imagen.src = "https://img.icons8.com/arcade/40/null/delete-sign.png"
+                    child.appendChild(imagen)
+
+                    imagen = document.createElement("img")
+                    imagen.src = "https://img.icons8.com/3d-plastilina/35/null/pencil--v2.png"
+                    child.appendChild(imagen)
+
+                    tr.appendChild(child)
+                    parent.appendChild(tr)
+                }
+            })
+        };
+        ul.appendChild(li);
     })
-};
-    ul.appendChild(li);
-})}
+}
 
-
-function DatosSites(){
+function DatosSites() {
     sitesArray.map(site => {
 
         let parent = document.getElementsByTagName('table')[0]
@@ -119,7 +117,7 @@ function DatosSites(){
         tr.appendChild(child)
         child = document.createElement('td')
 
-        imagen.src= "https://img.icons8.com/ultraviolet/40/null/opened-folder.png"
+        imagen.src = "https://img.icons8.com/ultraviolet/40/null/opened-folder.png"
         child.appendChild(imagen)
 
         imagen = document.createElement("img")
@@ -129,11 +127,18 @@ function DatosSites(){
         imagen = document.createElement("img")
         imagen.src = "https://img.icons8.com/3d-plastilina/35/null/pencil--v2.png"
         child.appendChild(imagen)
-        
+
         tr.appendChild(child)
         parent.appendChild(tr)
     })
 }
 
-
-
+function randomPassword() {
+    let caracteres = '1234567890qwertyuiopñlkjhgfdsazxcvbnmQWERTYUIOPÑLKJHGFDSAZXCVBNM'
+    let password = "";
+    for (let i = 0; i < 10; i++) {
+        let nCaracter = Math.floor(Math.random() * caracteres.length + 1);
+        password += caracteres.charAt(nCaracter)
+    }
+    return password
+}
